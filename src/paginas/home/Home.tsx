a6c1e5926c1a54014d947c5cb107bc45b5ecdd09
MariaJoaquina <mcaacmmatheus@gmail.com>
2022-06-29 15:27:30 -0300

    primeira estilização home, react, css

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home()
+{
+    return (
+    <>
+    <p className="titulo">Home</p>
+    <img className="imagemHome" src="https://wallpapers.com/images/high/city-buildings-in-rgb-v34bic1sbgzbtojp.jpg" alt="ImagemHome" />
+    </>
+    );
+}
+
+export default Home;
